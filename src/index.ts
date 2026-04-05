9ec27402553726578fcaba757289b1c0d9698b78
Sagni Amsalu <sanyii@Sanyii.localdomain>
2026-04-05 18:36:05 +0300

    resolve

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,21 @@ export { useAuth } from './hooks/useAuth';
 export { useProtectedRoute } from './hooks/useProtectedRoute';
 export { useAuthContext } from './components/AuthProvider';
 
-// Components
+// Components - ADD THE MISSING EXPORTS HERE
 export { AuthProvider } from './components/AuthProvider';
 export { ProtectedRoute } from './components/ProtectedRoute';
 export { LoginForm } from './components/LoginForm';
 export { MFASetup } from './components/MFASetup';
 export { MFAVerification } from './components/MFAVerification';
 export { SessionWarning } from './components/SessionWarning';
+export { AuthStatus } from './components/AuthProvider';
+export { SessionTimer } from './components/AuthProvider';
+export { SessionMonitor } from './components/AuthProvider';        
+export { AuthDebug } from './components/AuthProvider';            
+export { SessionExpiryWarning } from './components/AuthProvider'; 
+export { RoleBased } from './components/AuthProvider';            
+export { MFARequired } from './components/AuthProvider';          
+export { withAuth } from './components/AuthProvider';             
 
 // Interceptors
 export { setupAuthInterceptors } from './interceptors/axios-interceptors';
